59bfbfb65924b6c48c33ef02110f11dd80d66da9
hushcut <rechardmin@gmail.com>
2024-12-22 23:00:23 +0900

    파일이름 정하기 및 요일별 메뉴를 저장하는 컬렉션

--- src/CJUrestaurantApp.java
+++ src/CJUrestaurantApp.java
@@ -2,4 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class CJUrestaurantApp {
+    private static final String MENU_FILE = "menu.txt";
+    private static Map<String, List<String>> dailyMenus = new HashMap<>();
 }
