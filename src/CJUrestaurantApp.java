b6d9f59a3ac6c6424b7a687a3e8438c1ba2cf458
hushcut <rechardmin@gmail.com>
2024-12-23 18:55:17 +0900

    스캐너 추가

--- src/CJUrestaurantApp.java
+++ src/CJUrestaurantApp.java
@@ -3,7 +3,14 @@ import java.util.*;
 
 public class CJUrestaurantApp {
     private static final String MENU_FILE = "menu.txt";
+
     private static Map<String, List<String>> dailyMenus = new HashMap<>();
+
     public static void main(String[] args) {
         loadMenuFromFile();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+
 }
