0a42eee85435bcf1e7e0c0e829d0f69df7429a12
hushcut <rechardmin@gmail.com>
2024-12-23 18:49:44 +0900

    메뉴 파일에서 데이터를 로드

--- src/CJUrestaurantApp.java
+++ src/CJUrestaurantApp.java
@@ -4,4 +4,6 @@ import java.util.*;
 public class CJUrestaurantApp {
     private static final String MENU_FILE = "menu.txt";
     private static Map<String, List<String>> dailyMenus = new HashMap<>();
+    public static void main(String[] args) {
+        loadMenuFromFile();
 }
